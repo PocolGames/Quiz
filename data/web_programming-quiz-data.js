feb1628d6f273a0c715a3fcaeef9a5e2830eab0b
이은수 <dldmstn0409@gmail.com>
2025-07-30 02:12:47 +0900

    2

--- data/web_programming-quiz-data.js
+++ data/web_programming-quiz-data.js
@@ -1,17 +1,72 @@
-const rawQuizData = [
-    { question: "<!DOCTYPE html>", answer: "HTML5 문서임을 알리는 지시어" },
-    { question: "<html></html>", answer: "HTML 문서의 시작을 알리는 태그" },
-    { question: "<title></title>", answer: "문서의 제목을 정의하기" },
-    { question: "<h1></h1>", answer: "제목을 정의하기" },
-    { question: "<h1 title=\"안녕하세요\"에서 title의 의미는?", answer: "툴팁을 의미하는 속성" },
-    { question: "<p></p>", answer: "단락 나누기" },
-    { question: "<hr>", answer: "수평선 긋기" },
-    { question: "<br>", answer: "새로운 줄로 넘어가기" },
-    { question: "&입력하고 엔터티표현", answer: "문자, 기호, 심볼 입력하기" },
-    { question: "<pre></pre>", answer: "개발자의 포맷 그대로 출력" },
-    { question: "<b></b>", answer: "진하게" },
-    { question: "<strong></strong>", answer: "중요한" },
-    { question: "<em></em>", answer: "강조" },
-    { question: "<i></i>", answer: "이탤릭으로 강조" }
-  ];
-  
+let quizData = [
+    {
+        question: "<!DOCTYPE html>",
+        options: ["HTML5 문서임을 알리는 지시어", "HTML4 문서임을 알리는 지시어", "CSS 문서임을 알리는 지시어", "JavaScript 문서임을 알리는 지시어"],
+        answer: 1
+    },
+    {
+        question: "<html></html>",
+        options: ["HTML 문서의 시작을 알리는 태그", "CSS 문서의 시작을 알리는 태그", "JavaScript 문서의 시작을 알리는 태그", "PHP 문서의 시작을 알리는 태그"],
+        answer: 1
+    },
+    {
+        question: "<title></title>",
+        options: ["문서의 제목을 정의하기", "문서의 본문을 정의하기", "문서의 머리말을 정의하기", "문서의 바닥글을 정의하기"],
+        answer: 1
+    },
+    {
+        question: "<h1></h1>",
+        options: ["제목을 정의하기", "단락을 정의하기", "링크를 정의하기", "이미지를 정의하기"],
+        answer: 1
+    },
+    {
+        question: "<h1 title=\"안녕하세요\">에서 title의 의미는?",
+        options: ["툴팁을 의미하는 속성", "제목을 의미하는 속성", "링크를 의미하는 속성", "클래스를 의미하는 속성"],
+        answer: 1
+    },
+    {
+        question: "<p></p>",
+        options: ["단락 나누기", "제목 만들기", "링크 만들기", "이미지 삽입하기"],
+        answer: 1
+    },
+    {
+        question: "<hr>",
+        options: ["수평선 긋기", "수직선 긋기", "테이블 만들기", "리스트 만들기"],
+        answer: 1
+    },
+    {
+        question: "<br>",
+        options: ["새로운 줄로 넘어가기", "굵게 만들기", "밑줄 긋기", "색상 변경하기"],
+        answer: 1
+    },
+    {
+        question: "&를 입력하고 엔터티 표현하는 이유는?",
+        options: ["문자, 기호, 심볼 입력하기", "링크 만들기", "이미지 삽입하기", "테이블 만들기"],
+        answer: 1
+    },
+    {
+        question: "<pre></pre>",
+        options: ["개발자의 포맷 그대로 출력", "미리보기 출력", "압축된 형태로 출력", "암호화된 형태로 출력"],
+        answer: 1
+    },
+    {
+        question: "<b></b>",
+        options: ["진하게", "기울이게", "밑줄 긋기", "취소선 긋기"],
+        answer: 1
+    },
+    {
+        question: "<strong></strong>",
+        options: ["중요한", "일반적인", "부차적인", "선택적인"],
+        answer: 1
+    },
+    {
+        question: "<em></em>",
+        options: ["강조", "생략", "숨김", "삭제"],
+        answer: 1
+    },
+    {
+        question: "<i></i>",
+        options: ["이탤릭으로 강조", "굵게 강조", "밑줄로 강조", "색상으로 강조"],
+        answer: 1
+    }
+];
